7037e3f5a93c923ec801364608179dbec48dc6bb
ndnquang2010 <quangndn0585@ut.edu.vn>
2026-04-14 19:48:58 +0700

    Update SJF.h

--- CODE/SJF.h
+++ CODE/SJF.h
@@ -1,16 +1,31 @@
 #ifndef SJF_H
 #define SJF_H
 
+#include <iostream>
 #include <vector>
 #include <string>
 using namespace std;
 
-struct Process {
-    string id;
-    int at, bt;
-    int st, ct, wt, tat;
+struct ProcessSJF {
+    string name;
+    int arrivalTime;
+    int burstTime;
+
+    int startTime;
+    int finishTime;
+    int waitingTime;
+    int turnaroundTime;
+
+    bool completed = false;
 };
 
-vector<Process> sjf(vector<Process> p);
+// Đọc file CSV
+vector<ProcessSJF> readProcessesFromCSV_SJF(const string& filename);
+
+// SJF Non-Preemptive
+void sjfScheduling(vector<ProcessSJF>& processes);
+
+// In kết quả
+void printResults_SJF(const vector<ProcessSJF>& processes);
 
 #endif
