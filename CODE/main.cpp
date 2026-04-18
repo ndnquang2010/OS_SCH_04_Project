0dd9a1678e8f247143ac51e0d6aa5eaca475b435
ndnquang2010 <quangndn0585@ut.edu.vn>
2026-04-19 01:35:53 +0700

    Refactor main function for scheduling algorithms

--- CODE/main.cpp
+++ CODE/main.cpp
@@ -1,43 +1,64 @@
+#include <iostream>
+#include <vector>
 #include "SJF.h"
 #include "priority.h"
 
 using namespace std;
 
+// ---- chuyển ProcessPrio -> Process (cho SJF dùng chung input) ----
+vector<Process> convertToSJF(const vector<ProcessPrio>& prioList) {
+    vector<Process> sjfList;
+
+    for (const auto& p : prioList) {
+        Process temp;
+        temp.id = p.id;
+        temp.arrivalTime = p.arrivalTime;
+        temp.burstTime = p.burstTime;
+        sjfList.push_back(temp);
+    }
+
+    return sjfList;
+}
+
 int main() {
     string filename = "input.csv";
 
-    vector<ProcessPrio> processes = readProcessesFromCSV(filename);
+    // đọc dữ liệu 1 lần
+    vector<ProcessPrio> processesPrio = readProcessesFromCSV(filename);
 
-    if (processes.empty()) {
-        cout << "No process data found!" << endl;
-        return 1;
+    if (processesPrio.empty()) {
+        cout << "No data found or cannot read file.\n";
+        return 0;
     }
 
-    priorityScheduling(processes);
-    //printResults(processes);
+    int choice;
 
-    //B1: Đọc file CSV
-    vector<ProcessSJF> p = readProcessesFromCSV_SJF("input.csv");
+    cout << "===== CPU Scheduling Simulator =====\n";
+    cout << "1. Shortest Job First (SJF)\n";
+    cout << "2. Priority Scheduling\n";
+    cout << "Choose algorithm: ";
+    cin >> choice;
 
-    //kiểm tra đọc có thành công không
-    if (p.empty()) {
-        cout << "Khong doc duoc file CSV!\n";
-        return 1;
+    switch (choice) {
+        case 1: {
+            vector<Process> processesSJF = convertToSJF(processesPrio);
+
+            sjfScheduling(processesSJF);
+            printSJFResults(processesSJF);
+
+            break;
+        }
+
+        case 2: {
+            priorityScheduling(processesPrio);
+            printPriorityResults(processesPrio);
+
+            break;
+        }
+
+        default:
+            cout << "Invalid choice.\n";
     }
 
-    //B2: Chạy thuật toán SJF
-    sjfScheduling(p);
-/*
-    cout << "Result:\n";
-    for (auto &x : p) {
-        cout << x.name << "\t"
-             << "Arrival=" << x.arrivalTimeSjf << "\t"
-             << "Burst=" << x.burstTimeSjf << "\t"
-             << "Start=" << x.startTimeSjf << "\t"
-             << "Finish=" << x.finishTimeSjf << "\t"
-             << "Waiting=" << x.waitingTimeSjf << "\t"
-             << "Turnaround=" << x.turnaroundTimeSjf << endl;
-    }
-*/
     return 0;
 }
